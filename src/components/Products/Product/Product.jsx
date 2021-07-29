33d141bf4a8718ff007a0bea0b8639da1bc19c3c
Tiago <ti.volei@hotmail.com>
2021-07-29 02:54:59 -0300

    carrinho criado

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,32 +1,35 @@
 import React from 'react'
 import { Card, CardMedia, CardContent, CardActions, Typography, iconBotton, IconButton } from '@material-ui/core'
 import { AddShoppingCart } from '@material-ui/icons'
-import { classes } from 'istanbul-lib-coverage'
+
+import useStyle from './styles';
 
 // Classes de cartão para clientes  botão de carrinho sendo criado
-const Product = ({ product }) => {
+const Product = ({ product, onAddToCart }) => {
+    const classes = useStyles();
+
     return (
+        // Evento Botão do carrinho sendo criado
         <Card className={classes.root}>
             <Cardmedia className={classes.media} image='' title={product.name} />
             <CardContent>
                 <div className={classes.CardContent}>
-                    <Typography variant="h5" guetterBottom>
+                    <Typography guetterBottom variant="h5" component="h2">
                         {product.name}
                     </Typography>
-                    <Typography variant="h5">
-                        {product.price}
+                    <Typography guetterBottom variant="h5" component="h2">
+                        {product.price.formatted}
                     </Typography>
-                </div>
-                
-                <Typography variant='h2' color="textSecondary">{product.description}</Typography>
+                </div>                
+                <Typography dangerouslySetInnerHTML={{ __html: product.description }} variant="body2" color="textSecondary" component="p" />
             </CardContent>
             <cardActions disableSpacing classname={classes.cardActions}>
-                <IconButton aria-label="Add to cart"> 
+                <IconButton aria-label="Add to cart" onClick={handleAddToCart}> 
                     <AddShoppingCart />
                 </IconButton>
             </cardActions>
         </Card>
-    )
-}
+    );
+};
 
-export default Product
+export default Product;
---
+++ src/components/Products/Product/styles.js
@@ -0,0 +1,20 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+// Criando estilos para componentes Product
+export default makeStyles(() => ({
+  root: {
+    maxWidth: '100%',
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  cardActions: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+  cardContent: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+}));
